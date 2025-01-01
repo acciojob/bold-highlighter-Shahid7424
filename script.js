7a4c483075800f3000b11ad0245e845ba726449f
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2025-01-01 13:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function highlight() {
     //Write your code here
+	const boldWords = document.querySelectorAll("strong");
+	boldWords.forEach((word) => {
+		word.style.color = 'green';	
+	});
+}
+
+//  return 
+function return_normal (){
+	const boldWord = document.querySelectorAll('strong');
+	boldWord.forEach((word) =>{
+		word.style.color = "green";
+	});
+}
+
+	
+	
+	
+	
 
 
 }
